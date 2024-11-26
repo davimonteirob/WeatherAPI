7c5329dec43ac386063b34e2af327fe856c08ec8
Davi Monteiro <davi321522@gmail.com>
2024-11-26 13:08:17 -0300

    Att01

---
+++ WeatherAPI/Models/WeatherResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAPI.Models
+{
+    internal class WeatherResponse
+    {
+        public MainInfo Main { get; set; }
+
+        public Weather[] Weather { get; set; }
+    }
+}
---
+++ WeatherAPI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using WeatherAPI.Models;
+
+string apiKey = "ea20d5b8700db72131450c6ec7313a37";
+string cidade = "Paulista";
+string url = $"http://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={apiKey}&units=metric&lang=pt_br";
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        var resposta = await client.GetFromJsonAsync<WeatherResponse>(url);
+
+    } catch (Exception ex) 
+    {
+        Console.WriteLine(ex.Message);
+    }  
+}
